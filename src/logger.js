34444898d22bfffb9c66c2d419fa38cae953841b
abesic-starke <85228620+abesic-starke@users.noreply.github.com>
2021-07-21 10:01:01 +0200

    Updated logger console color
    The color 'blue' is very badly visible in Chrome dark consoles, updated it to 'skyblue'
    
    Example: https://pasteboard.co/KcarxRV.png

--- src/logger.js
+++ src/logger.js
@@ -28,7 +28,7 @@ export default new class VueSocketIOLogger {
 
     event(text, data = ''){
 
-        if(this.debug) window.console.info(this.prefix+`%c${text}`, 'color: blue; font-weight: 600', 'color: #333333', data);
+        if(this.debug) window.console.info(this.prefix+`%c${text}`, 'color: skyblue; font-weight: 600', 'color: #333333', data);
 
     }
 
